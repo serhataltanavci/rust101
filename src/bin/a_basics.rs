3b94156ff23e2a790a09f1cb92c857bff640576d
noob <noob@noobs-MacBook-Air.local>
2026-02-19 10:33:54 +0300

    learning rust on hard way

---
+++ src/bin/a_basics.rs
@@ -0,0 +1,16 @@
+fn main (){
+    // ! ok , this is a macro call
+    println!("hello there");
+}
+
+// ! compile etmek icin , rustc file_name.rs
+
+// ./file_name run
+
+// cargo run during dev,
+// eger sifirdan kendin rs dosyasi yaptiysan
+// ! cargo init ile projeye donusebilir
+
+// ! whithout run ====>>>> cargo check
+
+// cargo build --release///// with optimization
